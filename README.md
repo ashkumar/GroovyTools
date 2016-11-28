dd389e7a8c0ee334ce3a5c3364810d23690c9e26
Ashwini Kumar <kumar_ashni@yahoo.com>
2016-11-27 19:17:52 -0500

    First Version of SearchForClass.groovy

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GroovyTools
-Groovy based tools
+Groovy based tools:  The intent is to create essential tools in every Java developers warchest using the wonderful Groovy language.
+
+*SearchForClass.groovy*
+
+This class searches for a class recursively from a given directory supplied as a parameter.  It can even search for an inner class.  Some examples of usages are
+     
+     **groovy SearchForClass <Dir Name> <classname>**
+
+     **groovy SearchForClass /dev/apache-tomcat-7.0.70/lib PongMessage**
+
+           or if you know the full package name copied from the source
+
+     **groovy SearchForClass /dev/apache-tomcat-7.0.70/lib org.apache.tomcat.websocket.pojo.Constants**
+
+This groovy file demonstrates the versatility of the groovy closures and also the ease of using regular expressions
---
+++ SearchForClass.groovy
@@ -0,0 +1,45 @@
+import java.util.jar.JarFile;
+
+if (args.length != 2) {
+			System.out.println(
+				"Usage: groovy SearchForClass <Dir Name> <classname> \n" +
+				"eg. groovy SearchForClass /dev/apache-tomcat-7.0.70/lib PongMessage  or \n" +
+				" or if you know the full package name copied from the source \n" + 
+				"groovy SearchForClass /dev/apache-tomcat-7.0.70/lib org.apache.tomcat.websocket.pojo.Constants ");
+            System.exit(0);
+}
+//Directory to search recursively
+File dir = new File("${args[0]}")
+
+def className = "${args[1]}";
+def matchString
+def innerClassMatchString
+def isPackage = true;
+
+
+//If className contains a . then this is a full package name - must be handled differently
+if (className.contains(".")){
+	def packageName = className.replaceAll('\\.', '/');
+	matchString = packageName + ".class"+ "\$"
+} else {
+	isPackage = false
+	matchString = "[\\/\$]" + className + ".class"+ "\$"
+}
+
+println matchString
+
+JarFile jFile = null;
+dir.eachFileRecurse ({
+		String a =  it.absolutePath
+		if( a  =~ '.jar$'){
+			jFile = new JarFile(it);
+			jFile.entries().find ({
+				if (it.toString() =~ matchString){
+						println "Found ${it.toString()} in => " + jFile.name
+				}
+			})
+		}
+	})
+
+
+
